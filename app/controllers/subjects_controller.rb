53af77094d626e4016b5f5b4153bd30e99d887d3
Vijay Menon <vijaymenonx@gmail.com>
2015-01-07 21:30:21 -0800

    Added Create, Edit, Update and Delete actions for simple_cms

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,14 +5,58 @@ class SubjectsController < ApplicationController
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new({:name => "Default"})
+  end
+
+  def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+    # If save succeeds, redirect to the index action
+     redirect_to(:action => 'index')
+    # If save fails, redisplay the form so user can fix problems
+    else
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+    # If save succeeds, redirect to the index action
+     redirect_to(:action => 'show', :id => @subject.id)
+    # If save fails, redisplay the form so user can fix problems
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    redirect_to(:action => 'index')
+  end
+
+
+  private 
+
+    def subject_params
+      params.require(:subject).permit(:name, :position, :visible)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   #get 'demo/index'
   root "demo#index"
-  match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
